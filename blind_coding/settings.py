f8f8f95cf9ab4e209fced87caa9409f0deb25791
sankha555 <f20190029@pilani.bits-pilani.ac.in>
2021-03-21 00:52:39 +0530

    tests

--- blind_coding/settings.py
+++ blind_coding/settings.py
@@ -133,10 +133,10 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
 )
-STATIC_ROOT = os.path.join(REPOSITORY_ROOT, 'static/')
+STATIC_ROOT = os.path.join(REPOSITORY_ROOT, 'staticfiles')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(REPOSITORY_ROOT, 'media/')
+MEDIA_ROOT = os.path.join(REPOSITORY_ROOT, 'media')
 
 clientId = "222a2ef84f6881409d32ae21369d1a32"
 clientSecret = "67872757630a355db890ee74b6b20926cb9e025dbb444182df2bd2700fc64af1"
